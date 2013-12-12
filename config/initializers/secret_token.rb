7c9a22b054080e65a96b978c7db6e7a9df6a9f17
bredesen <brettrlewis@gmil.com>
2013-12-11 19:30:13 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MuseumApp::Application.config.secret_key_base = 'c2ae13e56da0ad682a7b37a0ad40871b08975efc4fd47a5f3dd06be4f590b398e935bbb054fd6f118859ef5d63b7d13806b49e6d6cb68269a88acd63ff9886bb'
